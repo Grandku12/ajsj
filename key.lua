96fc9b04e528a11c4836b8b30e9289519f48d30c
Grandku12 <andkuq@gmail.com>
2026-02-07 21:06:14 +0700

    Add Key: KEY-F1LRLXOM

--- key.lua
+++ key.lua
@@ -9,6 +9,7 @@ local valid_keys = {
     ["GRAND-TRIAL-1DAY"] = os.time() + 86400, -- Key 24 Jam dari sekarang
 
     ["KEY-DBMGX43M"] = 1770558449, -- User: 202005022
-    ["KEY-J11RA7RM"] = 1770558464, -- User: 202005022}
+    ["KEY-J11RA7RM"] = 1770558464, -- User: 202005022
+    ["KEY-F1LRLXOM"] = 1770559574, -- User: 202005022}
 
 return valid_keys
