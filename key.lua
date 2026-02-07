4a9aba2f483d8fd53a93257891b759a6b16781fb
Grandku12 <andkuq@gmail.com>
2026-02-07 20:47:46 +0700

    Add Key: KEY-J11RA7RM

--- key.lua
+++ key.lua
@@ -8,6 +8,7 @@ local valid_keys = {
     ["GRAND-DEMO-KEY"] = 2147483647, -- Key Permanen (Tahun 2038)
     ["GRAND-TRIAL-1DAY"] = os.time() + 86400, -- Key 24 Jam dari sekarang
 
-    ["KEY-DBMGX43M"] = 1770558449, -- User: 202005022}
+    ["KEY-DBMGX43M"] = 1770558449, -- User: 202005022
+    ["KEY-J11RA7RM"] = 1770558464, -- User: 202005022}
 
 return valid_keys
