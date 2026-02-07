7a638f9370e82a6990d85843f1e8902b2940d77a
Grandku12 <andkuq@gmail.com>
2026-02-07 20:47:31 +0700

    Add Key: KEY-DBMGX43M

--- key.lua
+++ key.lua
@@ -7,6 +7,7 @@ local valid_keys = {
     -- [AUTOSAVE_MARKER]
     ["GRAND-DEMO-KEY"] = 2147483647, -- Key Permanen (Tahun 2038)
     ["GRAND-TRIAL-1DAY"] = os.time() + 86400, -- Key 24 Jam dari sekarang
-}
+
+    ["KEY-DBMGX43M"] = 1770558449, -- User: 202005022}
 
 return valid_keys
