83cf940ae8c0b7186fcdb8fc90ebcb3e74df7752
Grandku12 <andkuq@gmail.com>
2026-02-08 16:28:28 +0700

    Remove user keys from key.lua
    Removed several user keys from the key.lua file.

--- apo/secret/key/nothing/heeh/nonono/panjangkan/key.lua
+++ apo/secret/key/nothing/heeh/nonono/panjangkan/key.lua
@@ -2,16 +2,6 @@ keys = {
  
  
 
-    ["KEY-LYHJSERM"] = 1770618123, -- User: 202005022
-
-    ["KEY-73BZIWIM"] = 1770622647, -- User: 202005022
-
-    ["KEY-GHOW1VMM"] = 1770622653, -- User: 202005022
-
-    ["KEY-EOXKF9NM"] = 2634536276, -- User: gra (10000 Days)
-
-
-
 
     ["GRANDKEY-FREE-KZI6N2RM"] = 1770628026, -- User: 202005022 | 9/2/2026
 
